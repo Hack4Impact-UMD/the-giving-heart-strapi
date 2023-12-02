40cc5c4fe513dc994e64b9e0165dbdfcf7ba2495
Alex Chen <achen131@umd.edu>
2023-12-01 19:30:50 -0500

    setup strapi email provider with ses

--- config/plugins.js
+++ config/plugins.js
@@ -16,4 +16,18 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  email: {
+    config: {
+      provider: 'amazon-ses',
+      providerOptions: {
+        key: env('AWS_SES_KEY'),
+        secret: env('AWS_SES_SECRET'),
+        amazon: 'https://email.us-east-2.amazonaws.com',
+      },
+      settings: {
+        defaultFrom: 'thegivingheart.dev@gmail.com',
+        defaultReplyTo: 'thegivingheart.dev@gmail.com',
+      },
+    },
+  },
 });
