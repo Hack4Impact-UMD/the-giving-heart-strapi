4e9bed70a6b6fd8ace8d95c5348b61a7909c022b
Dan Nguyen <danthedevnguyen@gmail.com>
2024-01-21 15:54:16 -0500

    waitlist collection and news banner

---
+++ src/api/news-banner/controllers/news-banner.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * news-banner controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::news-banner.news-banner');
