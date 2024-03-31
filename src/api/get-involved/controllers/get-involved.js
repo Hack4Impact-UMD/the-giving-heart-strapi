0bcc90d7dec7caf46d52466b58a8900095afdafc
Neha <101278033+neharath526@users.noreply.github.com>
2024-03-31 13:16:28 -0400

    get involved strapi (#4)
    * get involved strapi
    
    * add title for donate
    
    ---------
    
    Co-authored-by: Neha Rathnavel <nrathnav@terpmail.umd.com>

---
+++ src/api/get-involved/controllers/get-involved.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * get-involved controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::get-involved.get-involved');
